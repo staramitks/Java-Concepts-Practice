4a9693e37a51a063d7456093036eb7fe462cb15e
Amit Singh <star.amitks@gmail.com>
2022-06-26 19:22:59 +0530

    add for DP

---
+++ src/main/java/home/amit/java/exams/ChaseTest.java
@@ -0,0 +1,27 @@
+package home.amit.java.exams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class ChaseTest {
+
+    public void printSimilarWords(String input, List<String> inputList)
+    {
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        List<String> inputList= Arrays.asList("LOOP","MOON","COOK","APPLE","MANGO","TASTE");
+        String input="MOON";
+        ChaseTest chaseTest= new ChaseTest();
+        chaseTest.printSimilarWords(input,inputList);
+
+    }
+
+
+}
