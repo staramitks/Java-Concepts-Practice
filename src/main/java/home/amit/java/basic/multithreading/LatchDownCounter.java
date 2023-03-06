3d373591a5800c52130cd4f8a70197bbc49d0a3a
Amit Singh <star.amitks@gmail.com>
2023-03-06 23:13:19 +0530

    update latchdowncounter

---
+++ src/main/java/home/amit/java/basic/multithreading/LatchDownCounter.java
@@ -0,0 +1,9 @@
+package home.amit.java.basic.multithreading;
+
+public class LatchDownCounter {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello test");
+    }
+}
